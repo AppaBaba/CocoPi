d728ddaf333f9719fa31485904d3207b00027c20
AppaBaba <darleg@outlook.com>
2025-03-04 23:09:23 -0800

    add more C

---
+++ C/Add2Int/add2int.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {    
+
+    int n1 = 3;
+    int n2 = 2; 
+    int sum;
+    sum = n1 + n2;      
+    printf("%d + %d = %d \n", n1, n2, sum);
+    return 0;
+}
---
+++ C/Mul2Num/mul2num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    double a = 5;
+    double b = 3;
+    double product; 
+ 
+    product = a * b;
+
+    printf("Product = %.2lf \n", product);
+    
+    return 0;
+}
